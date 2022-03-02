ad35dd844e0e31da4f1471615bf2e519d3930d6b
Jordan Farelli <jordanfarelli@gmail.com>
2022-03-01 20:34:12 -0700

    Added details per instructions

---
+++ challenge/index.md
@@ -0,0 +1,3 @@
+<header>
+  <h1>"Book Titles"</h>
+  <p>"Science is Fun", "Astrology", "History", "Physical Fitness Practices", "Guide to Life".</p>
---
+++ challenge/pair_challenge.js
@@ -0,0 +1,3 @@
+//We chose to name the file pair_challenge.js, becasue it's a paired challenge.
+var books = ["Science Fiction", 1942, 1842, true, false, "Guide to Life"];
+books.push("Alice in Wonderland");
